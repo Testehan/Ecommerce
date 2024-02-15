7ae8b46f1abaf3bd6b16e90f4f0e7115583d80fd
DanTestehan <tdan89@yahoo.com>
2024-02-15 20:20:20 +0100

    save new address

--- parent/frontend/src/main/java/com/testehan/ecommerce/frontend/address/AddressController.java
+++ parent/frontend/src/main/java/com/testehan/ecommerce/frontend/address/AddressController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AddressController {
@@ -50,4 +52,26 @@ public class AddressController {
 
         return "address_book/address_form";
     }
+
+    @PostMapping("/address_book/save")
+    public String saveAddress(Address address, HttpServletRequest request, RedirectAttributes ra) throws CustomerNotFoundException {
+
+        var customer = Utility.getAuthenticatedCustomer(customerService, request);
+
+        address.setCustomer(customer);
+        addressService.save(address);
+
+        ra.addFlashAttribute("message", "The address has been saved successfully.");
+
+        var redirectOption = request.getParameter("redirect");
+        var redirectURL = "redirect:/address_book";
+
+        if ("checkout".equals(redirectOption)) {
+            redirectURL += "?redirect=checkout";
+        }
+
+        return redirectURL;
+
+    }
+
 }
