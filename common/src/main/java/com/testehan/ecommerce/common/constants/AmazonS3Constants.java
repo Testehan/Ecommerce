1fefcd702447d69cc0437d9b04dfb860c25cf084
DanTestehan <tdan89@yahoo.com>
2024-03-20 20:20:20 +0100

    use env variables to determine amazon S3 bucket

--- common/src/main/java/com/testehan/ecommerce/common/constants/AmazonS3Constants.java
+++ common/src/main/java/com/testehan/ecommerce/common/constants/AmazonS3Constants.java
@@ -1,2 +1,19 @@
-package com.testehan.ecommerce.common.constants;public class AmazonS3Constants {
+package com.testehan.ecommerce.common.constants;
+
+public class AmazonS3Constants {
+    public static final String S3_BASE_URI;
+
+    static {
+        var bucketName = System.getenv("AWS_BUCKET_NAME");
+        var region = System.getenv("AWS_REGION");
+        var pattern = "https://%s.s3.%s.amazonaws.com";
+
+        S3_BASE_URI = bucketName == null ? "" : String.format(pattern, bucketName, region);
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(S3_BASE_URI);
+    }
 }
