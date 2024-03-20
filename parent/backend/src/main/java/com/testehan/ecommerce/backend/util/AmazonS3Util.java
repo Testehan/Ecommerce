e1fc3ffa9ee22d4f6b7799c913377dc526488bd6
DanTestehan <tdan89@yahoo.com>
2024-03-20 20:20:20 +0100

    S3 upload file to bucket folder

--- parent/backend/src/main/java/com/testehan/ecommerce/backend/util/AmazonS3Util.java
+++ parent/backend/src/main/java/com/testehan/ecommerce/backend/util/AmazonS3Util.java
@@ -1,12 +1,18 @@
 package com.testehan.ecommerce.backend.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
@@ -14,6 +20,8 @@ import java.util.ListIterator;
 import static com.testehan.ecommerce.common.constants.AmazonS3Constants.*;
 
 public class AmazonS3Util {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AmazonS3Util.class);
 
     public static S3Client createAmazomS3() {
 
@@ -50,5 +58,26 @@ public class AmazonS3Util {
 
         return listKeys;
 
+    }
+
+    public static void uploadFile(String folderName, String fileName, InputStream inputStream) {
+
+        S3Client client = createAmazomS3();
+        PutObjectRequest request = putFileInAmazonS3(folderName, fileName);
+
+        try (inputStream) {
+            int contentLength = inputStream.available();
+            client.putObject(request, RequestBody.fromInputStream(inputStream, contentLength));
+
+        } catch (IOException ex) {
+            LOGGER.error("Could not upload file to Amazon S3", ex);
+        }
+    }
+
+    public static PutObjectRequest putFileInAmazonS3(String folderName, String fileName) {
+        PutObjectRequest request = PutObjectRequest.builder().bucket(BUCKET_NAME)
+                .key(folderName + "/" + fileName).acl("public-read").build();
+
+        return request;
     }
 }
--- parent/backend/src/test/java/com/testehan/ecommerce/backend/util/AmazonS3UtilTests.java
+++ parent/backend/src/test/java/com/testehan/ecommerce/backend/util/AmazonS3UtilTests.java
@@ -2,6 +2,10 @@ package com.testehan.ecommerce.backend.util;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 public class AmazonS3UtilTests {
@@ -11,5 +15,18 @@ public class AmazonS3UtilTests {
         var folderName = "test-upload";
         List<String> listKeys = AmazonS3Util.listFolder(folderName);
         listKeys.forEach(System.out::println);
+    }
+
+    @Test
+    public void uploadS3File() throws FileNotFoundException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("images/default-user.png").getFile());
+
+        var folderName = "test-upload";
+        var fileName = "default-user.png";
+
+        InputStream inputStream = new FileInputStream(file);
+
+        AmazonS3Util.uploadFile(folderName, fileName, inputStream);
     }
 }
